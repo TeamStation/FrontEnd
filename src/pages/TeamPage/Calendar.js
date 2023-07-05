fd422f017904ffcf43321bd5bf9508cbddb09212
ChaeYubin <cyoobin@naver.com>
2023-07-05 11:22:23 +0900

    Feat: 캘린더 라이브러리 추가

--- src/pages/TeamPage/Calendar.js
+++ src/pages/TeamPage/Calendar.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import CalendarComponent from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const Calendar = () => {
-  return <div className="calendar">캘린더 뷰</div>;
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div className="calendar">
+      <CalendarComponent onChange={onChange} value={value} />
+    </div>
+  );
 };
 
 export default Calendar;
