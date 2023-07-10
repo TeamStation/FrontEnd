c6b4fa1d22fe9c0b75f3c16e13491c6dee2a24b1
ChaeYubin <cyoobin@naver.com>
2023-07-10 13:57:24 +0900

    Design: 팀 페이지 왼쪽 사이드 바 팀원 프로필 사진 뷰 수정

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -8,13 +8,76 @@ const SideBar = ({ setPageView, setMainView, setMeetingNotesView }) => {
     setMeetingNotesView(true);
   };
 
+  const onClickMemberAddButton = () => {
+    console.log("팀원 추가 버튼 클릭");
+  };
+
+  const memberList = [
+    {
+      id: 1,
+      imageName: "member_thumbnail",
+    },
+    {
+      id: 2,
+      imageName: "member_thumbnail",
+    },
+    {
+      id: 3,
+      imageName: "member_thumbnail",
+    },
+    {
+      id: 4,
+      imageName: "member_thumbnail",
+    },
+    {
+      id: 5,
+      imageName: "member_thumbnail",
+    },
+  ];
+
   return (
     <div className="left-side-nav-bar side-bar">
       <div className="profile-image"></div>
       <div className="team-name">Team</div>
       <div className="member-profile-container">
-        <div className="members">팀원 사진들</div>
-        <div className="member-add-btn">Add</div>
+        <div className="members">
+          {memberList.map((member) => {
+            let move =
+              Math.abs(parseInt(memberList.length / 2 + 1) - member.id) *
+              20 *
+              -1;
+            return (
+              <img
+                className="profile-image"
+                key={member.id}
+                src={require(`../images/${member.imageName}.png`)}
+                alt="profile thumbnail"
+                style={
+                  member.id === parseInt(memberList.length / 2 + 1)
+                    ? {}
+                    : member.id < parseInt(memberList.length / 2 + 1)
+                    ? { position: "relative", right: `${move}px` }
+                    : { position: "relative", left: `${move}px` }
+                }
+              />
+            );
+          })}
+        </div>
+        <div
+          className="member-add-btn"
+          style={
+            memberList.length % 2 === 0
+              ? {
+                  position: "relative",
+                  left: `${5 * parseInt(memberList.length / 2) * -1}px`,
+                }
+              : {
+                  position: "relative",
+                  left: `${15 * parseInt(memberList.length / 2) * -1}px`,
+                }
+          }
+          onClick={onClickMemberAddButton}
+        ></div>
       </div>
       <div className="meeting-tab" onClick={onClickMeetingTab}>
         회의
