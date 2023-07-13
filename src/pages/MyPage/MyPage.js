4c610f1fbd3d5899ce84fd9e3a22e119331c60ba
yunhee1 <alkrg007@naver.com>
2023-07-13 13:21:22 +0900

    Feat: add mypage

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "../../css/my-page.scss";
+import ProjectStatus from "./ProjectStatus";
+import User from "./User";
+import Projects from "./Projects";
 
 function MyPage() {
-  return <div>MyPage</div>;
+  return (
+    <div class="my-page-container">
+      <ProjectStatus />
+      <User />
+      <Projects />{" "}
+    </div>
+  );
 }
 
 export default MyPage;
---
+++ src/pages/MyPage/Projects.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <>
+      <div className="project-container">
+        <h2 className="project-title-container">projects</h2>
+        <h1>프로젝트</h1>
+      </div>
+    </>
+  );
+};
+export default Projects;
