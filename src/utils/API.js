ad13d892d729f476b52efab657d88c3f4e901103
ChaeYubin <cyoobin@naver.com>
2023-07-15 01:10:26 +0900

    Feat: 로그인 기능 추가

---
+++ src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://3.35.203.183",
+  //   headers: {
+  // "Access-Control-Allow-Origin": `http://localhost:3000/`,
+  // "Access-Control-Allow-Credentials": "true",
+  //   },
+});
+
+export default API;
