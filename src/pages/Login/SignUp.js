a869c81e156137f484a6c464d8e876daa6ce451b
chaeyubin <cyoobin@naver.com>
2023-07-14 14:49:15 +0900

    Feat: 회원가입 기본 로직 추가

--- src/pages/Login/SignUp.js
+++ src/pages/Login/SignUp.js
@@ -1,40 +1,82 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import "../../css/signup-page.scss";
 
 function SignUp() {
+  const location = useLocation();
+  const email = location.state?.email;
+
+  const [nameInput, setNameInput] = useState("");
+  const [pwInput, setPwInput] = useState("");
+  const [confirmPwInput, setConfirmPwInput] = useState("");
+
+  const handleNameInput = (e) => {
+    setNameInput(e.target.value);
+  };
+
+  const handlePwInput = (e) => {
+    setPwInput(e.target.value);
+  };
+
+  const handleConfirmPwInput = (e) => {
+    setConfirmPwInput(e.target.value);
+  };
+
+  const onClick = () => {
+    if (pwInput !== confirmPwInput) {
+      alert("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
+      setPwInput("");
+      setConfirmPwInput("");
+    } else {
+      const request = {
+        email: email,
+        password: pwInput,
+        memberName: nameInput,
+        checkedPassword: confirmPwInput,
+      };
+
+      // API 요청 후 마이페이지로 이동
+    }
+  };
+
   return (
-    <div>
-      <div
-        style={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          flexDirection: "column",
-        }}
-      >
-        <h1>회원가입</h1>
-        <form style={{ display: "flex", flexDirection: "column" }}>
-          <input type="email" placeholder="이메일" />
-          <input type="name" placeholder="이름" />
-          <input type="password" placeholder="비밀번호" />
-          <input type="password" placeholder="비밀번호확인" />
-          <br />
-          <Link to="/mypage">
-            <button
-              formAction=""
-              style={{
-                backgroundColor: "#3D96FF",
-                borderRadius: "5px",
-                borderStyle: "none",
-                color: "#FFFFFF",
-              }}
-            >
-              회원가입
-            </button>
-          </Link>
-        </form>
+    <div className="signup-container">
+      <div>
+        <h1 className="signup-title">회원가입</h1>
+        <div className="signup-input-container">
+          <input
+            type="text"
+            placeholder={email}
+            className="input-box"
+            value={email}
+            onChange={null}
+            disabled={true}
+          />
+          <input
+            type="text"
+            placeholder="이름"
+            className="input-box"
+            value={nameInput}
+            onChange={handleNameInput}
+          />
+          <input
+            type="password"
+            placeholder="비밀번호"
+            className="input-box"
+            value={pwInput}
+            onChange={handlePwInput}
+          />
+          <input
+            type="password"
+            placeholder="비밀번호 확인"
+            className="input-box"
+            value={confirmPwInput}
+            onChange={handleConfirmPwInput}
+          />
+          <button className="signup-button" onClick={onClick}>
+            회원가입
+          </button>
+        </div>
       </div>
     </div>
   );
