536d71bf798e31f32ab15f97c5dcbe5431e9a773
ChaeYubin <cyoobin@naver.com>
2023-07-16 00:40:05 +0900

    Fix: API 포트 번호 추가

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://3.35.203.183",
+  baseURL: "http://3.35.203.183:8080",
   //   headers: {
   // "Access-Control-Allow-Origin": `http://localhost:3000/`,
   // "Access-Control-Allow-Credentials": "true",
